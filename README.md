04f45ac917111a67d53d63f6850b593e61a91bca
Fabio Ramos <fabio.ramos@outlook.es>
2022-03-10 11:54:27 -0600

    Ansible

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Inicial
+
+ssh -i C:\Users\fabio.ramos\.ssh\id_rsa_cp2 fabio.ramos@20.103.251.15
---
+++ ansible/deploy.sh
@@ -0,0 +1,4 @@
+# /bin/bahs
+
+ansible-playbook -i host.azure playbook-inicial.yaml
+# ansible-playbook -i host.azure -l master 02-playbook.yaml
