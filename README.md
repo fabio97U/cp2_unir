580114ae947d950389658addfdc28788d3c44b05
Fabio Ramos <fabio.ramos@outlook.es>
2022-03-10 13:44:53 -0600

    Correccion typo archivos

--- README.md
+++ README.md
@@ -2,4 +2,13 @@ Inicial
 
 ssh -i C:\Users\fabio.ramos\.ssh\id_rsa_cp2 fabio.ramos@20.103.251.15
 
+dnf install epel-release ansible git tree jq -y
+
+dnf install python36 -y
+
+git clone https://github.com/fabio97U/cp2_unir.git
+fabio97U
 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
+cd cp2_unir/ansible/
+
+ansible -i hosts.azure -m ping all
--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,8 +1,9 @@
 # /bin/bahs
 
-ansible-playbook -i hosts.azure playbook-users.yml
+ansible-playbook -i hosts.azure playbook-inicial.yaml
+ansible-playbook -i hosts.azure playbook-users.yaml
 ansible-playbook -i hosts.azure playbook-install-k8s.yaml
-ansible-playbook -i hosts.azure playbook-master.yml
-ansible-playbook -i hosts.azure playbook-join-workers.yml
+ansible-playbook -i hosts.azure playbook-master.yaml
+ansible-playbook -i hosts.azure playbook-join-workers.yaml
 
 # ansible-playbook -i host.azure -l master 02-playbook.yaml
