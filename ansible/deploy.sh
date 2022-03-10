39cdcbfba00e9147f7d5c553d731fbf6bd6d137f
Fabio Ramos <fabio.ramos@outlook.es>
2022-03-10 12:58:43 -0600

    Add k8s

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,4 +1,8 @@
 # /bin/bahs
 
-ansible-playbook -i hosts.azure playbook-inicial.yaml
+ansible-playbook -i hosts.azure playbook-users.yml
+ansible-playbook -i hosts.azure playbook-install-k8s.yaml
+ansible-playbook -i hosts.azure playbook-master.yml
+ansible-playbook -i hosts.azure playbook-join-workers.yml
+
 # ansible-playbook -i host.azure -l master 02-playbook.yaml
