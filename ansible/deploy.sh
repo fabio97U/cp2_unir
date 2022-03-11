0afba3fd87396b323d0ff7cfaf1ae01f153ec8f3
Fabio Ramos <fabio.ramos@outlook.es>
2022-03-10 19:24:12 -0600

    Desplegando ingress

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -18,3 +18,5 @@ ansible-playbook -i hosts.azure 03-playbook-tareas-master.yaml
 
 ansible-playbook -i hosts.azure 04-A-playbook-sdn-calico.yaml
 ansible-playbook -i hosts.azure 04-B-playbook-flannel.yaml
+
+ansible-playbook -i hosts.azure 05-desplegando-ingress.yaml
