bf6d04307c2f6a86ed9e91baa2e7f808314aeb41
Fabio Ramos <fabio.ramos@outlook.es>
2022-03-10 20:05:45 -0600

    add flannel-abrir-puertos

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -17,6 +17,8 @@ ansible-playbook -i hosts.azure 02-playbook-tareas-comunes-nodos.yaml
 ansible-playbook -i hosts.azure 03-playbook-tareas-master.yaml
 
 ansible-playbook -i hosts.azure 04-A-playbook-sdn-calico.yaml
-ansible-playbook -i hosts.azure 04-B-playbook-flannel.yaml
+ansible-playbook -i hosts.azure 04-B1-playbook-flannel.yaml
+ansible-playbook -i hosts.azure 04-B2-playbook-flannel.yaml
 
+    # ANTES QUE NADA REINICIAR MASTER
 ansible-playbook -i hosts.azure 05-desplegando-ingress.yaml
