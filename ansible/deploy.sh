a079d3f74547f7d7ddbe278f856f271447c7efc2
Fabio Ramos <fabio.ramos@outlook.es>
2022-03-10 18:53:14 -0600

    SDN Calico

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -15,3 +15,4 @@ ansible-playbook -i hosts.practica playbook_practica.yaml
 ansible-playbook -i hosts.azure 01-playbook-config-general.yaml
 ansible-playbook -i hosts.azure 02-playbook-tareas-comunes-nodos.yaml
 ansible-playbook -i hosts.azure 03-playbook-tareas-master.yaml
+ansible-playbook -i hosts.azure 04-playbook-sdn-calico.yaml
